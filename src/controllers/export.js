c6829343a9178fa2054d8fdd6efcf94f46890a6e
Marina Oliveira <marinaoliveira.r07@gmail.com>
2021-02-21 22:48:48 -0300

    fix: formatting in exported csv files.

--- src/controllers/export.js
+++ src/controllers/export.js
@@ -1,28 +1,65 @@
 const fastcsv = require("fast-csv");
 const fs = require("fs");
 const download = require("../repositories/export");
-const path = require("path");
+const iconv = require("iconv-lite");
+const json2csv = require("json2csv");
+const { Parser } = require("json2csv");
+const { AsyncParser } = require("json2csv");
+const json2csvParser = new AsyncParser({ delimiter: "\t" });
+const { parseAsync } = require("json2csv");
 
 const exportTable = async (ctx) => {
   const { table = null } = ctx.params;
   console.log(table);
-  const ws = fs.createWriteStream(__dirname + `/${table}.csv`);
+  const ws = iconv
+    .encodeStream("utf8")
+    .pipe(fs.createWriteStream(__dirname + `/${table}.csv`));
   const database_table = await download.exportTable(table);
 
   if (database_table) {
     const jsonData = JSON.parse(JSON.stringify(database_table));
-    fastcsv
-      .write(jsonData, { headers: true })
-      .on("finish", function () {
-        console.log(
-          `Postgres table ${table} exported to CSV file successfully.`
-        );
-      })
-      .pipe(ws);
 
-    ctx.body = fs.createReadStream(__dirname + `/${table}.csv`);
-    console.log(ctx.body);
-    ctx.attachment(__dirname + `/${table}.csv`);
+    let fields = [
+      "id",
+      "character_name",
+      "main_storyseries",
+      "author",
+      "genre",
+      "type_of_rep",
+      "gender",
+      "importance",
+      "sexual_orientation",
+      "romantic_orientation",
+      "relationships",
+      "pairing_qpp_or_romantic",
+      "rep_noteswarnings",
+      "cover",
+      "added_in",
+    ];
+    let csvStr = await parseAsync(jsonData);
+    fs.writeFileSync(
+      `${table}.csv`,
+      "\ufeff" + csvStr,
+      { encoding: "utf16le" },
+      function (err) {
+        if (err) throw err;
+        console.log("file saved");
+      }
+    );
+    // fastcsv
+    //   .write(jsonData, { headers: true, encoding: "binary" })
+    //   .on("finish", function () {
+    //     console.log(
+    //       `Postgres table ${table} exported to CSV file successfully.`
+    //     );
+    //   })
+    //   .pipe(ws);
+
+    ctx.body = fs.createReadStream(`${table}.csv`, {
+      encoding: "utf16le",
+    });
+    // console.log(ctx.body);
+    ctx.attachment(`./${table}.csv`);
     /// ??? send file ???
   }
 };
