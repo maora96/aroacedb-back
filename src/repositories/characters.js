d3ee621e98b0719d0e0ac799e8c89527bb485fee
Marina Oliveira <marinaoliveira.r07@gmail.com>
2021-02-24 20:27:27 -0300

    fix: search characters

--- src/repositories/characters.js
+++ src/repositories/characters.js
@@ -91,7 +91,17 @@ const searchCharacters = async (search) => {
   const queries = [];
   const stories_q = [];
   search.forEach((s) => {
-    const text = `SELECT * FROM characters WHERE character_name ILIKE '%${s}%' OR main_storyseries ILIKE '%${s}%' OR author ILIKE '%${s}%' OR genre ILIKE '%${s}%' OR type_of_rep ILIKE '%${s}%' OR gender ILIKE '%${s}%' OR importance ILIKE '%${s}%' OR sexual_orientation ILIKE '%${s}%' OR romantic_orientation ILIKE '%${s}%' OR relationships ILIKE '%${s}%' OR pairing_qpp_or_romantic ILIKE '%${s}%' OR rep_noteswarnings ILIKE '%${s}%' order by character_name asc `;
+    const text = `select distinct * from (SELECT * FROM characters WHERE character_name ILIKE '%${s}%' OR main_storyseries ILIKE '%${s}%' OR author ILIKE '%${s}%' OR genre ILIKE '%${s}%' OR type_of_rep ILIKE '%${s}%' OR gender ILIKE '%${s}%' OR importance ILIKE '%${s}%' OR sexual_orientation ILIKE '%${s}%' OR romantic_orientation ILIKE '%${s}%' OR relationships ILIKE '%${s}%' OR pairing_qpp_or_romantic ILIKE '%${s}%' OR rep_noteswarnings ILIKE '%${s}%' union  select * from "characters" c2 
+    where c2.id in (select distinct character_id from stories 
+    where story_title ilike '%${s}%'
+    or series_or_anthology ilike '%${s}%'
+    or genre ilike '%${s}%'
+    or story_length ilike '%${s}%'
+    or type_of_rep ilike '%${s}%'
+    or character_importance ilike '%${s}%'
+    or rep_noteswarnings ilike '%${s}%'
+    or other_noteswarnings ilike '%${s}%')) as css 
+    `;
     queries.push(text);
   });
 
@@ -104,6 +114,7 @@ const searchCharacters = async (search) => {
       formatted.push(q);
     }
   });
+  formatted.push(" order by character_name asc");
   const final = formatted.join("");
   const q = {
     text: final,
